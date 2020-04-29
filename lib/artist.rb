3db6f36c05d36bcae7a44581d49fec67f9a1170d
Stephen McBride <stevemcbride3@gmail.com>
2020-04-29 18:16:14 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,25 +1,25 @@
 require "pry"
-class Artist 
+class Artist
   attr_accessor :name, :songs
   @@all = []
-  
+
   def initialize(name)
     @name = name
     @songs = []
     @@all.push(self)
   end
-  
+
   def add_song_by_name(name)
     add_song(Song.new(name, self))
   end
-  
+
   def add_song(song)
     song.artist = self
     songs.push(song)
   end
-  
+
   def self.song_count
-    count = 0 
+    count = 0
     @@all.each {|artist| count += artist.songs.length}
     count
   end
